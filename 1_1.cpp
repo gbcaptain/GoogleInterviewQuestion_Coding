29bf999e638eeb0c9236504ebabf066e4701e591
Bruce <gbcaptain@gmail.com>
2018-07-08 22:13:10 -0400

    2nd update

--- 1_1.cpp
+++ 1_1.cpp
@@ -1,9 +1,14 @@
-public static boolean isUniqueChars2(String str) {
-  boolean[] char_set = new boolean[256];
-  for (int i = 0; i < str.length(); i++) {
-      int val = str.charAt(i);
-      if (char_set[val]) return false;
-      char_set[val] = true;
+// Implement an algorithm to determine if a string has
+// all unique characters. What if you can not use additional
+// data structures?
+
+
+  public static boolean isUniqueChars2(String str) {
+    boolean[] char_set = new boolean[256];
+    for (int i = 0; i < str.length(); i++) {
+        int val = str.charAt(i);
+        if (char_set[val]) return false;
+        char_set[val] = true;
+        }
+        return true;
       }
-      return true;
-    }
